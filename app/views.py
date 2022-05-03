65184273f350f8271f044e2977db5f06ef958ad7
moni-que <monicahjustus@gmail.com>
2022-05-03 12:08:12 +0300

    add api key

--- app/views.py
+++ app/views.py
@@ -1,12 +1,16 @@
 from flask import render_template
+from pip import main
 from app import app
 from newsapi import NewsApiClient
 
 # Views
 @app.route('/')
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
+    newsapi = NewsApiClient(api_key='0ea4e8f58b09419abc487cf7d995bfc9')
+
+
+
     return render_template('index.html')
